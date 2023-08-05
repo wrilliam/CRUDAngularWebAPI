2aab0d92cb5bbb8207d501341ca7b53bd693670d
W. R <williamribeiroibiapina@gmail.com>
2023-08-05 20:44:40 -0300

    Preparing the environment and creating model and context

---
+++ Backend/WebAPI/WebAPI/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class Contexto : DbContext
+    {
+        #region Atributos
+        public DbSet<Pessoa> Pessoas { get; set; }
+        #endregion
+
+        #region Construtor
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+        }
+        #endregion
+    }
+}
---
+++ Backend/WebAPI/WebAPI/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.Models
+{
+    public class Pessoa
+    {
+        public int IdPessoa { get; set; }
+        public string Tipo { get; set; }
+        public string Documento { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+    }
+}
