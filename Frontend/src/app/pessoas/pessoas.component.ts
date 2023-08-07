c783e513c0dbaff35092e89bb78d253e37d08b37
William Ibiapina <williamribeiroibiapina@gmail.com>
2023-08-07 18:55:30 -0300

    Deleting data

--- Frontend/src/app/pessoas/pessoas.component.ts
+++ Frontend/src/app/pessoas/pessoas.component.ts
@@ -1,4 +1,4 @@
-  import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit, TemplateRef } from '@angular/core';
   import { FormControl, FormGroup } from '@angular/forms';
   import { Pessoa } from '../Pessoa';
   import { PessoasService } from '../pessoas.service';
@@ -13,7 +13,7 @@
     formulario: any
     tituloFormulario!: string
     pessoas!: Pessoa[]
-    idPessoa!: number;
+    idPessoa!: number
 
     visivelTabela: boolean = true
     visivelFormulario: boolean = false
@@ -30,21 +30,21 @@
       const pessoa: Pessoa = this.formulario.value;
 
       if(pessoa.idPessoa > 0) {
-        this.pessoasService.Update(pessoa).subscribe((resultado) => {
+        this.pessoasService.Update(pessoa).subscribe(() => {
           this.visivelTabela = true
           this.visivelFormulario = false
           alert('Registro atualizado com sucesso!')
-          this.pessoasService.Select().subscribe((resultado) => {
+          this.pessoasService.Select().subscribe(resultado => {
             this.pessoas = resultado
           })
         })
       }
       else {
-        this.pessoasService.Insert(pessoa).subscribe((resultado) => {
+        this.pessoasService.Insert(pessoa).subscribe(() => {
           this.visivelTabela = true
           this.visivelFormulario = false
           alert('Registro inserido com sucesso!')
-          this.pessoasService.Select().subscribe((resultado) => {
+          this.pessoasService.Select().subscribe(resultado => {
             this.pessoas = resultado
           })
         })
@@ -73,7 +73,7 @@
       this.visivelTabela = false;
       this.visivelFormulario = true;
 
-      this.pessoasService.SelectById(idPessoa).subscribe((resultado) => {
+      this.pessoasService.SelectById(idPessoa).subscribe(resultado => {
         this.tituloFormulario = `Atualizando Registro de ${resultado.nome}, aka ${resultado.apelido}`
         this.formulario = new FormGroup({
           idPessoa: new FormControl(resultado.idPessoa),
@@ -86,4 +86,13 @@
       })
     }
 
+    ExcluirRegistro(idPessoa: number): void {
+      this.pessoasService.Delete(idPessoa).subscribe(() => {
+        alert("Registro excluído com sucesso!")
+        this.pessoasService.Select().subscribe(resultado => {
+          this.pessoas = resultado
+      })
+    })
   }
+
+}
